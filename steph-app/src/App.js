4bb1076247f122fdfec968a19b3e079d66a6760f
Chris Abey <chris.abey587@gmail.com>
2022-08-10 20:18:42 -0600

    made picture rendering capability

--- steph-app/src/App.js
+++ steph-app/src/App.js
@@ -5,6 +5,19 @@ import './App.css';
 
 function App() {
 
+  //this is for the new code
+  const [backStyle, setBackStyle] = useState({backgroundImage: 'url("https://64.media.tumblr.com/d0d35d169ce18a126a981c0711889e2a/31b08f0b993a723f-b6/s540x810/93f3b0bc85f70fbcc045c0384645c73b4a616083.gifv")'});
+
+  let found = false;
+
+  var dict = {
+    duranke: "https://64.media.tumblr.com/504d6ad1da286454599122f023ce211d/tumblr_ofzxluhCAr1ruj0bpo1_1280.jpg",
+    ewingpa: "https://i.imgur.com/y88b17k.jpeg"
+  };
+
+
+  //end of new code
+
   const [name, setName] = useState('');
   const [headingText, setHeadingText] = useState('NBA Player');
 
@@ -24,15 +37,46 @@ function App() {
         } else {
           formattedPlayer += name[i];
         } 
+
       }
 
+      //new code
+      let onePlayer = "";
+      let dummyBool = false;
+      let counter = 0;
+
+      for(var i = 0; i < name.length; i++){
+        
+        if(dummyBool && counter<5){
+            onePlayer += name[i].toLowerCase();
+            counter++;
+        }
+        
+        if(name[i] === " "){
+            dummyBool = true;
+        }  
+      }
+
+      onePlayer += name[0].toLowerCase();
+      onePlayer += name[1].toLowerCase();
+      console.log(onePlayer + "hiii")
+
+      for(var key in dict){
+        // console.log(key)
+        if(key === onePlayer){
+          console.log(dict[key])
+          found = true;
+          setBackStyle({ backgroundImage: 'url(' + dict[key] + ')' })
+        }
+      }
+      //end new code
       setHeadingText(formattedPlayer);
     }
   }
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={backStyle}>
 
         <input value={name} onKeyDown={handleKeyDown} onChange={handleChange} type="search" placeholder="Search for NBA Players"></input>
 
